302c44703a95ecb2ea3146ee805c7ed9a7ef4259
Vanessa Busch <vbusch@redhat.com>
2017-05-11 18:26:35 -0400

    To get the user token out of the config, the name needs to have '-' instead of '.'

--- scripts/ocenv.sh
+++ scripts/ocenv.sh
@@ -3,7 +3,9 @@ HOST=${1:-localhost}
 NAMESPACE=${2-myproject}
 USER=${3-developer}
 
+OC_CONFIG_NAME=$USER/`echo ${HOST} | sed -r 's/\./-/g'`
+
 export OPENSHIFT_USER=$USER
-export OPENSHIFT_TOKEN=`oc config view -o jsonpath="{.users[?(@.name == \"${USER}/${HOST}:8443\")].user.token}"`
+export OPENSHIFT_TOKEN=`oc config view -o jsonpath="{.users[?(@.name == \"${OC_CONFIG_NAME}:8443\")].user.token}"`
 export OPENSHIFT_MASTER_URL=https://${HOST}:8443
 export OPENSHIFT_NAMESPACE=${NAMESPACE}
